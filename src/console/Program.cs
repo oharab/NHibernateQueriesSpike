3b2bef13a7badafac98819b706bb4ad7aceffe2e
Ben O'Hara <bpo@robotparade.co.uk>
2012-08-28 13:28:31 +0100

    Initial commit. Basic WindsorEnabled console app.

---
+++ src/console/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * User: 721116
+ * Date: 28/08/2012
+ * Time: 12:43
+ * 
+ *  */
+using System;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+namespace console
+{
+	class Program
+	{
+		private static IWindsorContainer container;
+		
+		public static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Loading container");
+			
+			container=new WindsorContainer();
+			container.Install(
+				 FromAssembly.This()
+			);
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Completed.");
+			Console.ReadKey(true);
+			
+			container.Dispose();
+		}
+	}
+}
